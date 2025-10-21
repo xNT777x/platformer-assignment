88a84f532af140f0e5fb28a082563368906de73e
NT777 <xnt777x@gmail.com>
2025-10-21 12:24:10 +0700

    added coyote time

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 class_name Player
 
-enum State {MOVING, JUMPING, FALLING, IN_COYOTE, IDLING}
+enum State {MOVING, JUMPING, FALLING, IN_COYOTE, IDLING, ATTACK, HURT, DEATH}
 
 @onready var player_animation: AnimatedSprite2D = %PlayerAnimation
 
 var current_state: PlayerState = null
 var state_factory := PlayerStateFactory.new()
-var facing
+var facing := "right"
 
 @export var speed: int = 500
 @export var gravity: int = 2000
@@ -17,6 +17,8 @@ func _ready() -> void:
 	switch_state(State.IDLING)
 
 func _physics_process(_delta: float) -> void:
+	handle_direction()
+	flip_sprite()
 	move_and_slide()
 
 func switch_state(state: State) ->  void:
@@ -28,3 +30,15 @@ func switch_state(state: State) ->  void:
 	current_state.state_transition_requested.connect(switch_state.bind())
 	current_state.name = "PlayerStateMachine: " + str(state)
 	call_deferred("add_child", current_state)
+
+func handle_direction() ->  void:
+	if velocity.x > 0:
+		facing = "right"
+	elif velocity.x < 0:
+		facing = "left"
+
+func flip_sprite() -> void:
+	if facing == "left":
+		player_animation.flip_h = true
+	else:
+		player_animation.flip_h = false
